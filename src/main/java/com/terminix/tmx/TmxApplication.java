5df1b757f178d628ac588ca9c31ae345831e419c
CheonYohan <kero74@naver.com>
2022-03-15 09:11:29 +0900

    init

---
+++ src/main/java/com/terminix/tmx/TmxApplication.java
@@ -0,0 +1,13 @@
+package com.terminix.tmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terminix/tmx/TmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminix.tmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
